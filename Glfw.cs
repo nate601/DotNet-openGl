1cd1750a5530472312d8046ef29602d6330e75dd
Nathan Button <nathanebutton@gmail.com>
2020-03-29 17:44:56 +0000

    Initial Commit

---
+++ Glfw.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace GlBindings
+{
+    static class Glfw
+    {
+        #region Delegates
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public delegate void ErrorFunc(int errorCode, string description);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public delegate void KeyCallback(IntPtr window, int key, int scanCode, int action, int mods);
+        #endregion
+        #region functions
+        [DllImport("glfw", EntryPoint = "glfwInit")]
+        public static extern bool Init();
+        [DllImport("glfw", EntryPoint = "glfwTerminate")]
+        public static extern void Terminate();
+        [DllImport("glfw", EntryPoint = "glfwSetErrorCallback")]
+        public static extern ErrorFunc SetErrorCallback(ErrorFunc callback);
+        [DllImport("glfw", EntryPoint = "glfwCreateWindow")]
+        public static extern IntPtr CreateWindow(int width, int height, string title, IntPtr monitor = default(IntPtr), IntPtr share = default(IntPtr));
+        [DllImport("glfw", EntryPoint = "glfwWindowShouldClose")]
+        public static extern bool WindowShouldClose(IntPtr window);
+        [DllImport("glfw", EntryPoint = "glfwGetTime")]
+        public static extern double GetTime();
+        [DllImport("glfw", EntryPoint = "glfwSetKeyCallback")]
+        public static extern KeyCallback SetKeyCallback(IntPtr window, KeyCallback callback);
+        [DllImport("glfw", EntryPoint = "glfwMakeContextCurrent")]
+        public static extern void MakeContextCurrent(IntPtr window);
+        [DllImport("glfw", EntryPoint = "glfwGetProcAddress", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr GetGlFunctionAddress(string processName);
+	[DllImport("glfw", EntryPoint = "glfwExtensionSupported")]
+	public static extern bool IsGlExtensionSupported(string extensionName);
+	[DllImport("glfw", EntryPoint = "glfwPollEvents")]
+	public static extern void PollEvents();
+        #endregion
+    }
+    static class Gl
+    {
+        public static void LoadGl()
+        {
+	    _GetGlString = GetGlMethod<glGetGlString>("glGetString");
+        }
+        private static T GetGlMethod<T>(string procName)
+        {
+	    Console.WriteLine($"Attempting to find function {procName} for delegate {typeof(T).Name}");
+            var pointer = Glfw.GetGlFunctionAddress(procName);
+            if (pointer == IntPtr.Zero)
+            {
+                Console.WriteLine($"FAILURE: Unable to find function {procName} for delegate {typeof(T).Name}");
+                return default(T);
+            }
+            Console.WriteLine($"SUCCESS: Pointer {procName} for delegate {typeof(T).Name} is {pointer}");
+	    return Marshal.GetDelegateForFunctionPointer<T>(pointer);
+        }
+#region internalFunctions
+        private static glDrawArrays _DrawArrays;
+	private static glGetGlString _GetGlString;
+#endregion
+#region Delegates
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        internal delegate void glDrawArrays(int mode, int first, int count);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+	internal delegate IntPtr glGetGlString(int gl_reference);
+        #endregion
+        #region ExternalFunctions
+        public static string GetGlString(int gl_reference) => Marshal.PtrToStringAuto(_GetGlString(gl_reference));
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using GlBindings;
+
+namespace openGlTest
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Glfw.SetErrorCallback(ErrorCallback);
+            if (Glfw.Init())
+            {
+                Console.WriteLine("Glfw has successfully initialized");
+            }
+            else
+            {
+                Console.WriteLine("Glfw has failed to successfully initialize");
+                return -1;
+            }
+            var window = Glfw.CreateWindow(640, 480, ".NET Core GL");
+            if (window == IntPtr.Zero)
+            {
+                Console.WriteLine("Error creating context window.");
+                return -1;
+            }
+            Glfw.MakeContextCurrent(window);
+            Gl.LoadGl();
+
+            {
+                const int GL_VENDOR = 0x1F00;
+                Console.WriteLine($"OpenGL reports the vendor responsible for this implementation as: {Gl.GetGlString(GL_VENDOR)}");
+            }
+
+            while (!Glfw.WindowShouldClose(window))
+            {
+
+		Glfw.PollEvents();
+            }
+
+            Glfw.Terminate();
+            return 0;
+        }
+        public static void ErrorCallback(int errorCode, string description)
+        {
+            Console.WriteLine($"ERROR:{errorCode} : {description}");
+        }
+    }
+}
