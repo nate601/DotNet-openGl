0a6fe9e9628b02ed4c16ddbd9e4fab89c7541224
Nathan Button <nathanebutton@gmail.com>
2020-09-24 05:18:34 +0000

    Create shader object templates

---
+++ Shader.cs
@@ -0,0 +1,45 @@
+using System.IO;
+
+namespace GlBindings
+{
+    public abstract class ShaderBase
+    {
+        internal uint shaderIdentifier;
+        public abstract void Compile();
+        public abstract void Init();
+        public abstract string ShaderSource { set; }
+
+        public void SetShaderSource(string shaderSource)
+        {
+            ShaderSource = shaderSource;
+        }
+        public void LoadShaderSourceFromFile(string filePath)
+        {
+            string v = File.ReadAllText(filePath);
+            ShaderSource = v;
+        }
+    }
+
+
+    public class VertexShader : ShaderBase
+    {
+        private const int ShaderType = 0x8B31;
+
+        public override string ShaderSource
+        {
+            set => Gl.ShaderSource(shaderIdentifier, value);
+        }
+
+        public override void Compile()
+        {
+            throw new System.NotImplementedException();
+
+        }
+
+        public override void Init()
+        {
+            shaderIdentifier = Gl.CreateShader(ShaderType);
+        }
+    }
+
+}
