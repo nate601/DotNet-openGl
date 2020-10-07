5bded9e1ce5c2d8a1f204f9ced921e03594af4ec
Nathan Button <nathanebutton@gmail.com>
2020-10-07 02:19:33 +0000

    Change fragment shader to update color based on time

--- Fragment_Shader.glsl
+++ Fragment_Shader.glsl
@@ -1,5 +1,8 @@
 #version 330 core
+
 out vec4 frag_colour;
+uniform float time;
+
 void main() {
-  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);
+  frag_colour = vec4(0.5, sin(time) / 2.0 + 0.5 , 0.5, 1.0);
 }
