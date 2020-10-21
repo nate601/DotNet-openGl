e4451c1013c32073ebdcb68b84fc5bd0ed294bc1
Nathan Button <nathanebutton@gmail.com>
2020-10-21 06:38:13 +0000

    Remove trailing whitespace

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@ namespace openGlTest
             _ = Glfw.SetKeyCallback(window, Marshal.GetFunctionPointerForDelegate(keyCallbackDelegate));
 
             float[] vertices = new float[]{
-            //position           texture 
+            //position           texture
              0.5f,  0.5f, 0.0f,  1.0f, 1.0f, // top    right
              0.5f, -0.5f, 0.0f,  1.0f, 0.0f, // bottom right
             -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, // bottom left
