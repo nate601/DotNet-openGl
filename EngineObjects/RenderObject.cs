3e76ec12b3b3235ceca7696a7e50e2f71a2b49a2
Nathan Button <nathanebutton@gmail.com>
2020-11-25 01:32:00 -0600

    Add EngineObject classes
    RenderObject: Represents an object that will be rendered by the Renderer
    class
    
    Transform: Holds a Vector3D Position, a rotation, and other information
    to represent the location of a sprite
    
    Camera: Represents the location of a camera's location within the scene.
    The main camera is stored in a static variable.
    
    Renderer: Performs the matrix multiplication to get the correct render
    position for the RenderObjects.

--- EngineObjects/RenderObject.cs
+++ EngineObjects/RenderObject.cs
@@ -1,4 +1,29 @@
+using GlBindings;
+
 namespace openGlTest.EngineObjects
 {
-
+    public class RenderObject
+    {
+        public Transform transform;
+    }
+    public class Transform
+    {
+        public Vector3D position;
+    }
+    public class Camera
+    {
+        public static Camera mainCamera;
+        public Transform transform;
+        public Camera()
+        {
+            if(mainCamera is null)
+            {
+                mainCamera = this;
+            }
+        }
+    }
+    public static class Renderer
+    {
+        
+    }
 }
