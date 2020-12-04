89abca1b36dd34693268e9803074d169e32ed9c1
Nathan Button <nathanebutton@gmail.com>
2020-12-04 01:07:51 -0600

    Add grid class

--- EngineObjects/Grid.cs
+++ EngineObjects/Grid.cs
@@ -2,10 +2,16 @@ namespace GameGrid
 {
     public class Grid
     {
-
+        public GridItem[,] gridObjects = new GridItem[20,20];
     }
     public class GridItem
     {
+        public readonly int x,y;
+        public GridItem(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
 
     }
 }
