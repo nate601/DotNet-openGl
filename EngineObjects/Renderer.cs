9d622ae8e1a15d917474770fe5a94babd36ddf19
Nathan Button <nathanebutton@gmail.com>
2020-12-04 01:08:49 -0600

    Cache perspectiveProjection

--- EngineObjects/Renderer.cs
+++ EngineObjects/Renderer.cs
@@ -7,21 +7,19 @@ namespace openGlTest.EngineObjects
     {
         public static List<ShaderProgram> shaderPrograms = new List<ShaderProgram>();
         public static List<Sprite> subscribeToRender = new List<Sprite>();
+        public static readonly float[,] perspectiveProjection = MatrixProjections.GetPerspectiveProjection(45, 640, 480, 0.1f, 100);
         public static void EndScene()
         {
 
         }
         public static void Render(Sprite renderObject, Camera camera)
         {
-            var shaderProgram = renderObject.shader;
-            shaderProgram.Bind();
+            renderObject.shader.Bind();
             renderObject.tex.SetActiveTexture(0);
             renderObject.buffers.vao.Bind();
-            /* Console.WriteLine(renderObject.transform.GetModelMatrix().ToStringPretty()); */
-            /* Console.WriteLine(camera.transform.GetModelMatrix().ToStringPretty()); */
-            shaderProgram.SetUniform("model", renderObject.transform.GetModelMatrix());
-            shaderProgram.SetUniform("view", camera.transform.GetModelMatrix());
-            shaderProgram.SetUniform("projection", MatrixProjections.GetPerspectiveProjection(45, 640, 480, 0.1f, 100));
+            renderObject.shader.SetUniform("model", renderObject.transform.GetModelMatrix());
+            renderObject.shader.SetUniform("view", camera.transform.GetModelMatrix());
+            renderObject.shader.SetUniform("projection", perspectiveProjection);
             Gl.DrawElements(0x004, 6, 0x1405, 0);
         }
         public static void Update()
